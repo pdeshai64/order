2812b35d548579bdf756c5b190740f09be1e8e76
pratik.deshai <pradikdeshai64@gmail.com>
2024-11-15 14:36:40 +0530

    Init commit for order creation

--- src/test/java/com/order/order/OrderServiceTest.java
+++ src/test/java/com/order/order/OrderServiceTest.java
@@ -2,16 +2,44 @@ package com.order.order;
 
 import com.order.order.dto.OrderDto;
 import com.order.order.dto.OrderItemDto;
+import com.order.order.entity.Order;
 import com.order.order.entity.OrderItem;
+import com.order.order.entity.OrderStatus;
+import com.order.order.repo.OrderItemRepo;
+import com.order.order.repo.OrderRepo;
+import com.order.order.service.OrderService;
+import com.order.order.service.OrderServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+
 @SpringBootTest
 public class OrderServiceTest {
+
+    @Mock
+    OrderItemRepo orderItemRepo;
+
+    @Mock
+    OrderRepo orderRepo;
+
+    OrderServiceImpl orderService;
+
+    @BeforeEach
+    void init() {
+        orderService  = new OrderServiceImpl(orderRepo, orderItemRepo);
+    }
 
     @Test
     void testCreateOrderMethod() {
@@ -21,8 +49,20 @@ public class OrderServiceTest {
         Mockito.when(orderItemDto.getProductCode()).thenReturn("abc");
 
         OrderDto orderDto = new OrderDto();
-        //orderDto.setItems(List.of(orderItemDto));
+        orderDto.setItems(List.of(orderItemDto));
 
+        orderService.createOrder(orderDto);
+
+        ArgumentCaptor<Order> orderArgumentCaptor = ArgumentCaptor.forClass(Order.class);
+        Mockito.verify(orderRepo, times(2)).save(orderArgumentCaptor.capture());
+        assertEquals(orderArgumentCaptor.getValue().getStatus(), OrderStatus.CREATED);
+
+        ArgumentCaptor<List<OrderItem>> listArgumentCaptor = ArgumentCaptor.forClass(List.class);
+        Mockito.verify(orderItemRepo, times(1)).saveAll(listArgumentCaptor.capture());
+        OrderItem orderItem = listArgumentCaptor.getValue().get(0);
+        assertEquals(BigDecimal.ONE, orderItem.getUnitPrice());
+        assertEquals("abc", orderItem.getProductCode());
+        assertEquals(1, orderItem.getQuantity());
 
     }
 }
